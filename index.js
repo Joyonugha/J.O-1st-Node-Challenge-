621b7885951d0ea548a6fb9bde9090cce07c37dd
Joy Onugha <joyonugha@gmail.com>
2024-02-23 03:35:39 +0000

    changed 'title' to name as inquirer may not recongise this

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const fs = require("fs");
-const path = require('path');
+const path = require('node:path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-let questions = [
+const questions = [
     
   {
     type: 'input',
-    title: 'title',
+    name: 'title',
     message: 'What is the title of your project?',
   },
   {
     type: 'input',
-    title: 'sections entitled',
+    name: 'sections entitled',
     message: 'Are you able to list the questions entitled(Yes or No)?',
   },
   {
     type: 'input',
-    title: 'description',
+    name: 'description',
     message: 'Can you please describe your project?',
   },
   {
     type: 'input',
-    title: 'table of content',
+    name: 'table of content',
     message: 'Could you please list the table of contents in the order that it appears?',
   },
   {
     type: 'input',
-    title: 'installation',
+    name: 'installation',
     message: 'How do you install it',
   },
   {
     type: 'input',
-    title: 'usage',
+    name: 'usage',
     message: 'How do you use it?',
   },
   {
     type: 'list',
-    title: 'license',
+    name: 'license',
     message: 'What license would you like to use for this project?',
     choices: ['MIT', 'Apache  2.0', 'GPL v3', 'Other'],
   },
   {
     question: 'input',
-    title: 'contributing',
+    name: 'contributing',
     message: 'If you would like to contrubute please follow these steps',
   },
   {
     type: 'input',
-    title: 'tests',
+    name: 'tests',
     message: 'How can your project be tested?',
   },
   {
     type: 'input',
-    title: 'questions',
+    name: 'questions',
     message: 'if you have any questions please follow these steps and',
   },
   
